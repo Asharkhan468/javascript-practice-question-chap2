9c212a176ef54e4484ce14a690891e4dd3c60465
Asharkhan468 <asharullah.khan2468@gmail.com>
2024-02-28 00:16:13 +0500

    practice set Chap2 done

---
+++ app.js
@@ -0,0 +1,52 @@
+// Chapter2 variable for string started
+
+// Qusetion 1
+var userName="Ali Ahmed";
+
+alert(userName);
+
+// Qusetion 2
+var myName="Ashar Khan";
+
+alert(myName);
+
+// Qusetion 3
+var message="Hello world";
+
+alert(message);
+
+// Qusetion 4
+
+var Name="Zohaib";
+var FatherName="Jabar";
+var rollNo= 24556;
+var Class="matric";
+
+
+alert(Name );
+alert(FatherName);
+alert(rollNo);
+alert(Class);
+
+// Qusetion 5
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// Qusetion 6
+
+var email="asharkhan@gmail.com";
+
+alert("My email is "+ email);
+
+// Qusetion 7
+
+var book="A smarter way to learn javascript";
+
+alert("I am trying to learn from the book " + book);
+
+// Qusetion 8
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
+// Chapter2 variable for string started
